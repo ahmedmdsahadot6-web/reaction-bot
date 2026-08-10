a95027872d5bce79318bb3be1710f6a7e8d53dc6
ahmedmdsahadot6-web <ahmedmdsahadot6@gmail.com>
2026-08-10 22:22:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ def keep_alive():
     t_ping.start()
 
 # 🔑 Configuration
-BOT_TOKEN = "8320025447:AAFWnP_asWXs6WXS-h_gPAy6Baikd6-4jMc"
-BOT_USERNAME = "@TGSUPER_SERVICE_BOT"
+BOT_TOKEN = "8895135409:AAFcEL-TULxTbjil0BNO_hX38oddGlEdlIw"
+BOT_USERNAME = "@Sahadot_reaction123_bot"
 ADMIN_IDS = [8454401183, 7871224176]
 ADMIN_USERNAME = "@SOYABUR_AS_LEADER"
 
 # 📢 Order Logs Channel
-LOG_CHANNEL = "@orderchannelsuperfast"
+LOG_CHANNEL = "@vucctx"
 
 # 🌐 Default SMM Panel Config
 SMM_API_URL = "https://1xpanel.com/api/v2"
