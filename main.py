f636fdac6106677ce66e448a9858f6720d8be77a
ahmedmdsahadot6-web <ahmedmdsahadot6@gmail.com>
2026-08-23 09:14:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def keep_alive():
     t_ping.start()
 
 # 🔑 Configuration
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8895135409:AAFcEL-TULxTbjil0BNO_hX38oddGlEdlIw")
-BOT_USERNAME = "@Sahadot_reaction123_bot"
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8976277256:AAFmnjs1mR_BQXm5Inwtki-M-RmO0O2P6Fc")
+BOT_USERNAME = "@FastAutoReact_bot"
 ADMIN_IDS = [8454401183, 7871224176]
 ADMIN_USERNAME = "@SOYABUR_AS_LEADER"
 
